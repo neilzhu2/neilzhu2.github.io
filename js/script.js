d06ee32f036fbc4ac1018c4472d4026a00b03861
neilzhu2 <neilzhu@umich.edu>
2023-06-19 16:00:45 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
-var button = document.getElementById("button");
+// var button = document.getElementById("button");
+//
+//
+//
+// button.addEventListener("click", ()=>{
+//   // console.log(button.innerHTML);
+//
+//   // button.innerHTML = 1;
+//
+//   let innerNum = button.innerHTML;
+//   button.innerHTML = Number(innerNum) + 1;
+// })
 
 
+// Get elements to interact
+const interactiveObjects = document.querySelectorAll(".interactiveObject");
 
-button.addEventListener("click", ()=>{
-  // console.log(button.innerHTML);
+const pin = document.querySelector("#pin");
+const note = document.querySelector("#note");
 
-  // button.innerHTML = 1;
 
-  let innerNum = button.innerHTML;
-  button.innerHTML = Number(innerNum) + 1;
+interactiveObjects.forEach((object) => {
+  // object.addEventListener('mouseenter',
+  //    function(){
+  //       this.setAttribute("opacity","0.9");
+  //       console.log(this);
+  // });
+  //
+  // object.addEventListener('mouseleave',
+  //    function(){
+  //       this.setAttribute("opacity", "1");
+  // });
+
+  pin.addEventListener('click',
+   function(){
+      this.setAttribute("opacity", "0.5")
+      note.setAttribute("opacity", "1")
+  });
+
 })
-
-
-// button.
