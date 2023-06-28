70741d18e66496a092f9ddb87cdcd53bff74a9cc
neilzhu2 <neilzhu@umich.edu>
2023-06-28 09:56:35 -0700

    update

--- js/script_prototype.js
+++ js/script_prototype.js
@@ -12,7 +12,7 @@ const inputField = document.querySelector("#input");
 
 var hasStartedComment = false;
 
-console.log("version 1.15");
+console.log("version 1.16");
 
 
 interactiveObjects.forEach((object) => {
