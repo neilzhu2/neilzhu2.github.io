afa213b15b74c5a4b0e0f079aeef8a6a4e493405
neilzhu2 <neilzhu@umich.edu>
2023-06-27 18:24:39 -0700

    update

--- js/script_prototype.js
+++ js/script_prototype.js
@@ -1,15 +1,3 @@
-// var button = document.getElementById("button");
-//
-//
-//
-// button.addEventListener("click", ()=>{
-//   // console.log(button.innerHTML);
-//
-//   // button.innerHTML = 1;
-//
-//   let innerNum = button.innerHTML;
-//   button.innerHTML = Number(innerNum) + 1;
-// })
 
 
 // Get elements to interact
